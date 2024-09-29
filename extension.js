123cd7c50f88dd1dc73c1255b8b02e34c642a013
Artem Babii <arthem.babij@gmail.com>
2024-09-29 11:44:13 +0300

    add gnome 47 support

--- extension.js
+++ extension.js
@@ -1,6 +1,6 @@
 import { Extension } from "resource:///org/gnome/shell/extensions/extension.js";
 import * as Main from "resource:///org/gnome/shell/ui/main.js";
-import * as Config from 'resource:///org/gnome/shell/misc/config.js';
+import * as Config from "resource:///org/gnome/shell/misc/config.js";
 import Clutter from "gi://Clutter";
 
 const QUICK_SETTINGS_NAME = "system";
@@ -8,92 +8,125 @@ const PLACEHOLDER_NAME = "unlikely-placeholder-name";
 const MIN_LENGTH = 3;
 
 export default class LineupExtension extends Extension {
-    constructor(metadata) {
-        super(metadata);
-        this._settings = null;
-        this._actorsAddId = null;
-        this._actorsChangeId = null;
-        this._timeoutIds = [];
-    }
+  constructor(metadata) {
+    super(metadata);
+    this._settings = null;
+    this._actorsAddId = null;
+    this._actorsChangeId = null;
+    this._timeoutIds = [];
+  }
 
-    _processActor(actor) {
-        const delaySec = this._settings.get_int("delay-sec") * 1000;
-        const timeoutId = setTimeout(() => {
-            const excludeWords = this._settings.get_string("exclude-words").toLowerCase() || PLACEHOLDER_NAME;
-            const hideWords = this._settings.get_string("hide-words").toLowerCase() || PLACEHOLDER_NAME;
+  _processActor(actor) {
+    const delaySec = this._settings.get_int("delay-sec") * 1000;
+    const timeoutId = setTimeout(() => {
+      const excludeWords =
+        this._settings.get_string("exclude-words").toLowerCase() ||
+        PLACEHOLDER_NAME;
+      const hideWords =
+        this._settings.get_string("hide-words").toLowerCase() ||
+        PLACEHOLDER_NAME;
 
-            const actorName = actor.get_first_child().get_accessible_name().toLowerCase() || PLACEHOLDER_NAME;
-            const actorObjectName = actor.get_first_child().toString().toLowerCase();
+      const actorName =
+        actor.get_first_child().get_accessible_name().toLowerCase() ||
+        PLACEHOLDER_NAME;
+      const actorObjectName = actor.get_first_child().toString().toLowerCase();
 
-            if (
-                actorName === QUICK_SETTINGS_NAME ||
-                excludeWords.split(/\s+/).some((word) => word.length >= MIN_LENGTH && actorName.includes(word)) ||
-                excludeWords.split(/\s+/).some((word) => word.length >= MIN_LENGTH && actorObjectName.includes(word))
-            ) {
-                return;
-            }
+      if (
+        actorName === QUICK_SETTINGS_NAME ||
+        excludeWords
+          .split(/\s+/)
+          .some(
+            (word) => word.length >= MIN_LENGTH && actorName.includes(word),
+          ) ||
+        excludeWords
+          .split(/\s+/)
+          .some(
+            (word) =>
+              word.length >= MIN_LENGTH && actorObjectName.includes(word),
+          )
+      ) {
+        return;
+      }
 
-            if (
-                hideWords.split(/\s+/).some((word) => word.length >= MIN_LENGTH && actorName.includes(word)) ||
-                hideWords.split(/\s+/).some((word) => word.length >= MIN_LENGTH && actorObjectName.includes(word))
-            ) {
-                actor.hide();
-                return;
-            }
+      if (
+        hideWords
+          .split(/\s+/)
+          .some(
+            (word) => word.length >= MIN_LENGTH && actorName.includes(word),
+          ) ||
+        hideWords
+          .split(/\s+/)
+          .some(
+            (word) =>
+              word.length >= MIN_LENGTH && actorObjectName.includes(word),
+          )
+      ) {
+        actor.hide();
+        return;
+      }
 
-            const desiredWidth = this._settings.get_int("indicator-width");
-            const minWidth = this._settings.get_int("min-width");
-            const maxWidth = this._settings.get_int("max-width");
+      const desiredWidth = this._settings.get_int("indicator-width");
+      const minWidth = this._settings.get_int("min-width");
+      const maxWidth = this._settings.get_int("max-width");
 
-            const actorWidth = actor.get_width();
-            if (actorWidth < minWidth || actorWidth > maxWidth) {
-                return;
-            }
+      const actorWidth = actor.get_width();
+      if (actorWidth < minWidth || actorWidth > maxWidth) {
+        return;
+      }
 
-            const realActor = actor.get_first_child();
-            if (realActor.get_first_child().get_style_class_name().includes("system-status-icon")) {
-                realActor.get_first_child().remove_style_class_name("system-status-icon");
-                realActor.get_first_child().add_style_class_name("popup-menu-icon");
-            }
+      const realActor = actor.get_first_child();
+      if (
+        realActor
+          .get_first_child()
+          .get_style_class_name()
+          .includes("system-status-icon")
+      ) {
+        realActor
+          .get_first_child()
+          .remove_style_class_name("system-status-icon");
+        realActor.get_first_child().add_style_class_name("popup-menu-icon");
+      }
 
-            realActor.set_width(desiredWidth);
-            realActor.get_first_child().set_x_align(Clutter.ActorAlign.CENTER);
-        }, delaySec);
-        this._timeoutIds.push(timeoutId);
-    }
+      realActor.set_width(desiredWidth);
+      realActor.get_first_child().set_x_align(Clutter.ActorAlign.CENTER);
+    }, delaySec);
+    this._timeoutIds.push(timeoutId);
+  }
 
-    async _processAllActors() {
-        const actors = Main.panel._rightBox.get_children();
-        const promises = actors.map(actor => this._processActor(actor));
-        await Promise.all(promises);
-    }
+  async _processAllActors() {
+    const actors = Main.panel._rightBox.get_children();
+    const promises = actors.map((actor) => this._processActor(actor));
+    await Promise.all(promises);
+  }
 
-    enable() {
-        this._settings = this.getSettings();
+  enable() {
+    this._settings = this.getSettings();
 
-        this._processAllActors();
+    this._processAllActors();
 
-        const shellVersion = parseFloat(Config.PACKAGE_VERSION).toString().slice(0, 2);
-        const signal = (shellVersion == 45) ? "actor-added" : "child-added";
+    const shellVersion = parseFloat(Config.PACKAGE_VERSION)
+      .toString()
+      .slice(0, 2);
+    const signal = shellVersion == 45 ? "actor-added" : "child-added";
 
-        this._actorsAddId = Main.panel._rightBox.connect(signal, (_, actor) => {
-            this._processActor(actor);
-        });
+    this._actorsAddId = Main.panel._rightBox.connect(signal, (_, actor) => {
+      this._processActor(actor);
+    });
 
-        this._actorsChangeId = this._settings.connect("changed", () => {
-            this._processAllActors();
-        });
-    }
+    this._actorsChangeId = this._settings.connect("changed", () => {
+      this._processAllActors();
+    });
+  }
 
-    disable() {
-        Main.panel._rightBox.disconnect(this._actorsAddId);
-        this._settings.disconnect(this._actorsChangeId);
-        this._settings = null;
-        this._actorsAddId = null;
-        this._actorsChangeId = null;
-        this._timeoutIds.forEach((timeoutId) => {
-            clearTimeout(timeoutId);
-        });
-        this._timeoutIds = [];
-    }
+  disable() {
+    Main.panel._rightBox.disconnect(this._actorsAddId);
+    this._settings.disconnect(this._actorsChangeId);
+    this._settings = null;
+    this._actorsAddId = null;
+    this._actorsChangeId = null;
+    this._timeoutIds.forEach((timeoutId) => {
+      clearTimeout(timeoutId);
+    });
+    this._timeoutIds = [];
+  }
 }
--- prefs.js
+++ prefs.js
@@ -4,192 +4,193 @@ import Gio from "gi://Gio";
 import Gtk from "gi://Gtk";
 
 export default class LineupPreferences extends ExtensionPreferences {
-    fillPreferencesWindow(window) {
-        const settings = this.getSettings();
-        const page = new Adw.PreferencesPage();
-        const groupMain = new Adw.PreferencesGroup();
-        const groupTarget = new Adw.PreferencesGroup({
-            title: "Target Group",
-            description: "Choose which indicators to resize",
-        });
-        const groupTweak = new Adw.PreferencesGroup({
-            title: "Advanced Tweaks",
-            description: "Fine-tuning for specific cases",
-        });
-        page.add(groupMain);
-        page.add(groupTarget);
-        page.add(groupTweak);
+  fillPreferencesWindow(window) {
+    const settings = this.getSettings();
+    const page = new Adw.PreferencesPage();
+    const groupMain = new Adw.PreferencesGroup();
+    const groupTarget = new Adw.PreferencesGroup({
+      title: "Target Group",
+      description: "Choose which indicators to resize",
+    });
+    const groupTweak = new Adw.PreferencesGroup({
+      title: "Advanced Tweaks",
+      description: "Fine-tuning for specific cases",
+    });
+    page.add(groupMain);
+    page.add(groupTarget);
+    page.add(groupTweak);
 
-        // Indicator Width
-        const rowWidth = new Adw.ActionRow({
-            title: "Indicator Width",
-            subtitle: "Select desired width for panel indicators",
-        });
-        groupMain.add(rowWidth);
+    // Indicator Width
+    const rowWidth = new Adw.ActionRow({
+      title: "Indicator Width",
+      subtitle: "Select desired width for panel indicators",
+    });
+    groupMain.add(rowWidth);
 
-        const widthAdjustment = new Gtk.Adjustment({
-            value: settings.get_int("indicator-width"),
-            lower: 40,
-            upper: 60,
-            step_increment: 1,
-        });
+    const widthAdjustment = new Gtk.Adjustment({
+      value: settings.get_int("indicator-width"),
+      lower: 40,
+      upper: 60,
+      step_increment: 1,
+    });
 
-        const widthSpinButton = new Gtk.SpinButton({
-            adjustment: widthAdjustment,
-            numeric: true,
-            valign: Gtk.Align.CENTER,
-            halign: Gtk.Align.END,
-        });
+    const widthSpinButton = new Gtk.SpinButton({
+      adjustment: widthAdjustment,
+      numeric: true,
+      valign: Gtk.Align.CENTER,
+      halign: Gtk.Align.END,
+    });
 
-        settings.bind(
-            "indicator-width",
-            widthSpinButton.get_adjustment(),
-            "value",
-            Gio.SettingsBindFlags.DEFAULT
-        );
+    settings.bind(
+      "indicator-width",
+      widthSpinButton.get_adjustment(),
+      "value",
+      Gio.SettingsBindFlags.DEFAULT,
+    );
 
-        rowWidth.add_suffix(widthSpinButton);
-        rowWidth.activatable_widget = widthSpinButton;
+    rowWidth.add_suffix(widthSpinButton);
+    rowWidth.activatable_widget = widthSpinButton;
 
-        // Min Width
-        const rowMinWidth = new Adw.ActionRow({
-            title: "Minimal Width",
-        });
-        groupTarget.add(rowMinWidth);
+    // Min Width
+    const rowMinWidth = new Adw.ActionRow({
+      title: "Minimal Width",
+    });
+    groupTarget.add(rowMinWidth);
 
-        const widthMinAdjustment = new Gtk.Adjustment({
-            value: settings.get_int("min-width"),
-            lower: 40,
-            upper: 60,
-            step_increment: 1,
-        });
+    const widthMinAdjustment = new Gtk.Adjustment({
+      value: settings.get_int("min-width"),
+      lower: 40,
+      upper: 60,
+      step_increment: 1,
+    });
 
-        const widthMinSpinButton = new Gtk.SpinButton({
-            adjustment: widthMinAdjustment,
-            numeric: true,
-            valign: Gtk.Align.CENTER,
-            halign: Gtk.Align.END,
-        });
+    const widthMinSpinButton = new Gtk.SpinButton({
+      adjustment: widthMinAdjustment,
+      numeric: true,
+      valign: Gtk.Align.CENTER,
+      halign: Gtk.Align.END,
+    });
 
-        settings.bind(
-            "min-width",
-            widthMinSpinButton.get_adjustment(),
-            "value",
-            Gio.SettingsBindFlags.DEFAULT
-        );
+    settings.bind(
+      "min-width",
+      widthMinSpinButton.get_adjustment(),
+      "value",
+      Gio.SettingsBindFlags.DEFAULT,
+    );
 
-        rowMinWidth.add_suffix(widthMinSpinButton);
-        rowMinWidth.activatable_widget = widthMinSpinButton;
+    rowMinWidth.add_suffix(widthMinSpinButton);
+    rowMinWidth.activatable_widget = widthMinSpinButton;
 
-        // Max Width
-        const rowMaxWidth = new Adw.ActionRow({
-            title: "Maximal Width",
-        });
-        groupTarget.add(rowMaxWidth);
+    // Max Width
+    const rowMaxWidth = new Adw.ActionRow({
+      title: "Maximal Width",
+    });
+    groupTarget.add(rowMaxWidth);
 
-        const widthMaxAdjustment = new Gtk.Adjustment({
-            value: settings.get_int("max-width"),
-            lower: 40,
-            upper: 60,
-            step_increment: 1,
-        });
+    const widthMaxAdjustment = new Gtk.Adjustment({
+      value: settings.get_int("max-width"),
+      lower: 40,
+      upper: 60,
+      step_increment: 1,
+    });
 
-        const widthMaxSpinButton = new Gtk.SpinButton({
-            adjustment: widthMaxAdjustment,
-            numeric: true,
-            valign: Gtk.Align.CENTER,
-            halign: Gtk.Align.END,
-        });
+    const widthMaxSpinButton = new Gtk.SpinButton({
+      adjustment: widthMaxAdjustment,
+      numeric: true,
+      valign: Gtk.Align.CENTER,
+      halign: Gtk.Align.END,
+    });
 
-        settings.bind(
-            "max-width",
-            widthMaxSpinButton.get_adjustment(),
-            "value",
-            Gio.SettingsBindFlags.DEFAULT
-        );
+    settings.bind(
+      "max-width",
+      widthMaxSpinButton.get_adjustment(),
+      "value",
+      Gio.SettingsBindFlags.DEFAULT,
+    );
 
-        rowMaxWidth.add_suffix(widthMaxSpinButton);
-        rowMaxWidth.activatable_widget = widthMaxSpinButton;
+    rowMaxWidth.add_suffix(widthMaxSpinButton);
+    rowMaxWidth.activatable_widget = widthMaxSpinButton;
 
-        // Exclude
-        const rowExclude = new Adw.ActionRow({
-            title: "Exclude Indicators",
-            subtitle: "Min length for each keyword is 3 letters",
-        });
-        groupTweak.add(rowExclude);
+    // Exclude
+    const rowExclude = new Adw.ActionRow({
+      title: "Exclude Indicators",
+      subtitle: "Min length for each keyword is 3 letters",
+    });
+    groupTweak.add(rowExclude);
 
-        const excludeEntry = new Gtk.Entry({
-            placeholder_text: "keyboard clipboard",
-            text: settings.get_string("exclude-words"),
-            valign: Gtk.Align.CENTER,
-            hexpand: true,
-        });
+    const excludeEntry = new Gtk.Entry({
+      placeholder_text: "keyboard clipboard",
+      text: settings.get_string("exclude-words"),
+      valign: Gtk.Align.CENTER,
+      hexpand: true,
+    });
 
-        settings.bind(
-            "exclude-words",
-            excludeEntry,
-            "text",
-            Gio.SettingsBindFlags.DEFAULT
-        );
+    settings.bind(
+      "exclude-words",
+      excludeEntry,
+      "text",
+      Gio.SettingsBindFlags.DEFAULT,
+    );
 
-        rowExclude.add_suffix(excludeEntry);
-        rowExclude.activatable_widget = excludeEntry;
+    rowExclude.add_suffix(excludeEntry);
+    rowExclude.activatable_widget = excludeEntry;
 
-        // Hide
-        const rowHide = new Adw.ActionRow({
-            title: "Hide Indicators",
-            subtitle: "Min length for each keyword is 3 letters",
-        });
-        groupTweak.add(rowHide);
+    // Hide
+    const rowHide = new Adw.ActionRow({
+      title: "Hide Indicators",
+      subtitle: "Min length for each keyword is 3 letters",
+    });
+    groupTweak.add(rowHide);
 
-        const hideEntry = new Gtk.Entry({
-            placeholder_text: "keepass",
-            text: settings.get_string("hide-words"),
-            valign: Gtk.Align.CENTER,
-            hexpand: true,
-        });
+    const hideEntry = new Gtk.Entry({
+      placeholder_text: "keepass",
+      text: settings.get_string("hide-words"),
+      valign: Gtk.Align.CENTER,
+      hexpand: true,
+    });
 
-        settings.bind(
-            "hide-words",
-            hideEntry,
-            "text",
-            Gio.SettingsBindFlags.DEFAULT
-        );
+    settings.bind(
+      "hide-words",
+      hideEntry,
+      "text",
+      Gio.SettingsBindFlags.DEFAULT,
+    );
 
-        rowHide.add_suffix(hideEntry);
-        rowHide.activatable_widget = hideEntry;
+    rowHide.add_suffix(hideEntry);
+    rowHide.activatable_widget = hideEntry;
 
-        // Resize Delay
-        const rowDelay = new Adw.ActionRow({
-            title: "Delay",
-            subtitle: "Increase if some indicators glitch or refuse to follow the rules",
-        });
-        groupTweak.add(rowDelay);
+    // Resize Delay
+    const rowDelay = new Adw.ActionRow({
+      title: "Delay",
+      subtitle:
+        "Increase if some indicators glitch or refuse to follow the rules",
+    });
+    groupTweak.add(rowDelay);
 
-        const delayAdjustment = new Gtk.Adjustment({
-            value: settings.get_int("delay-sec"),
-            lower: 0,
-            upper: 5,
-            step_increment: 1,
-        });
+    const delayAdjustment = new Gtk.Adjustment({
+      value: settings.get_int("delay-sec"),
+      lower: 0,
+      upper: 5,
+      step_increment: 1,
+    });
 
-        const delaySpinButton = new Gtk.SpinButton({
-            adjustment: delayAdjustment,
-            numeric: true,
-            valign: Gtk.Align.CENTER,
-            halign: Gtk.Align.END,
-        });
+    const delaySpinButton = new Gtk.SpinButton({
+      adjustment: delayAdjustment,
+      numeric: true,
+      valign: Gtk.Align.CENTER,
+      halign: Gtk.Align.END,
+    });
 
-        settings.bind(
-            "delay-sec",
-            delaySpinButton.get_adjustment(),
-            "value",
-            Gio.SettingsBindFlags.DEFAULT
-        );
+    settings.bind(
+      "delay-sec",
+      delaySpinButton.get_adjustment(),
+      "value",
+      Gio.SettingsBindFlags.DEFAULT,
+    );
 
-        rowDelay.add_suffix(delaySpinButton);
-        rowDelay.activatable_widget = delaySpinButton;
+    rowDelay.add_suffix(delaySpinButton);
+    rowDelay.activatable_widget = delaySpinButton;
 
-        window.add(page);
-    }
+    window.add(page);
+  }
 }
